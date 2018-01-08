ce658768169c0dc04aa0a07b65860174847a956e
guanzhisong <guanzhisong@gmail.com>
2018-01-08 16:05:10 +0800

    modify arch

--- rocketmq-console/src/main/java/org/apache/rocketmq/console/util/JsonUtil.java
+++ rocketmq-console/src/main/java/org/apache/rocketmq/console/util/JsonUtil.java
@@ -1,21 +1,27 @@
 /*
- * Licensed to the Apache Software Foundation (ASF) under one or more
- * contributor license agreements.  See the NOTICE file distributed with
- * this work for additional information regarding copyright ownership.
- * The ASF licenses this file to You under the Apache License, Version 2.0
- * (the "License"); you may not use this file except in compliance with
- * the License.  You may obtain a copy of the License at
+ * Licensed to the Apache Software Foundation (ASF) under one or more contributor license
+ * agreements. See the NOTICE file distributed with this work for additional information regarding
+ * copyright ownership. The ASF licenses this file to You under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance with the License. You may obtain a
+ * copy of the License at
  *
- *     http://www.apache.org/licenses/LICENSE-2.0
+ * http://www.apache.org/licenses/LICENSE-2.0
  *
- *  Unless required by applicable law or agreed to in writing, software
- *  distributed under the License is distributed on an "AS IS" BASIS,
- *  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- *  See the License for the specific language governing permissions and
- *  limitations under the License.
+ * Unless required by applicable law or agreed to in writing, software distributed under the License
+ * is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express
+ * or implied. See the License for the specific language governing permissions and limitations under
+ * the License.
  */
 
 package org.apache.rocketmq.console.util;
+
+import java.io.IOException;
+import java.io.Writer;
+import java.text.SimpleDateFormat;
+import java.util.Map;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 import com.fasterxml.jackson.annotation.JsonInclude;
 import com.fasterxml.jackson.core.type.TypeReference;
@@ -25,21 +31,14 @@ import com.fasterxml.jackson.databind.SerializationFeature;
 import com.fasterxml.jackson.databind.ser.impl.SimpleFilterProvider;
 import com.google.common.base.Strings;
 import com.google.common.base.Throwables;
-import java.io.IOException;
-import java.io.Writer;
-import java.text.SimpleDateFormat;
-import java.util.Map;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 
-@SuppressWarnings("unchecked")
+@SuppressWarnings({"unchecked", "deprecation"})
 public class JsonUtil {
 
     private static Logger logger = LoggerFactory.getLogger(JsonUtil.class);
     private static ObjectMapper objectMapper = new ObjectMapper();
 
-    private JsonUtil() {
-    }
+    private JsonUtil() {}
 
     static {
         objectMapper.configure(SerializationFeature.FAIL_ON_EMPTY_BEANS, false);
@@ -54,8 +53,7 @@ public class JsonUtil {
     public static void writeValue(Writer writer, Object obj) {
         try {
             objectMapper.writeValue(writer, obj);
-        }
-        catch (IOException e) {
+        } catch (IOException e) {
             Throwables.propagateIfPossible(e);
         }
     }
@@ -66,9 +64,8 @@ public class JsonUtil {
         }
 
         try {
-            return src instanceof String ? (String)src : objectMapper.writeValueAsString(src);
-        }
-        catch (Exception e) {
+            return src instanceof String ? (String) src : objectMapper.writeValueAsString(src);
+        } catch (Exception e) {
             logger.error("Parse Object to String error src=" + src, e);
             return null;
         }
@@ -80,9 +77,8 @@ public class JsonUtil {
         }
 
         try {
-            return src instanceof byte[] ? (byte[])src : objectMapper.writeValueAsBytes(src);
-        }
-        catch (Exception e) {
+            return src instanceof byte[] ? (byte[]) src : objectMapper.writeValueAsBytes(src);
+        } catch (Exception e) {
             logger.error("Parse Object to byte[] error", e);
             return null;
         }
@@ -94,10 +90,10 @@ public class JsonUtil {
         }
         str = escapesSpecialChar(str);
         try {
-            return clazz.equals(String.class) ? (T)str : objectMapper.readValue(str, clazz);
-        }
-        catch (Exception e) {
-            logger.error("Parse String to Object error\nString: {}\nClass<T>: {}\nError: {}", str, clazz.getName(), e);
+            return clazz.equals(String.class) ? (T) str : objectMapper.readValue(str, clazz);
+        } catch (Exception e) {
+            logger.error("Parse String to Object error\nString: {}\nClass<T>: {}\nError: {}", str,
+                    clazz.getName(), e);
             return null;
         }
     }
@@ -107,10 +103,10 @@ public class JsonUtil {
             return null;
         }
         try {
-            return clazz.equals(byte[].class) ? (T)bytes : objectMapper.readValue(bytes, clazz);
-        }
-        catch (Exception e) {
-            logger.error("Parse byte[] to Object error\nbyte[]: {}\nClass<T>: {}\nError: {}", bytes, clazz.getName(), e);
+            return clazz.equals(byte[].class) ? (T) bytes : objectMapper.readValue(bytes, clazz);
+        } catch (Exception e) {
+            logger.error("Parse byte[] to Object error\nbyte[]: {}\nClass<T>: {}\nError: {}", bytes,
+                    clazz.getName(), e);
             return null;
         }
     }
@@ -121,11 +117,12 @@ public class JsonUtil {
         }
         str = escapesSpecialChar(str);
         try {
-            return (T)(typeReference.getType().equals(String.class) ? str : objectMapper.readValue(str, typeReference));
-        }
-        catch (Exception e) {
-            logger.error("Parse String to Object error\nString: {}\nTypeReference<T>: {}\nError: {}", str,
-                typeReference.getType(), e);
+            return (T) (typeReference.getType().equals(String.class) ? str
+                    : objectMapper.readValue(str, typeReference));
+        } catch (Exception e) {
+            logger.error(
+                    "Parse String to Object error\nString: {}\nTypeReference<T>: {}\nError: {}",
+                    str, typeReference.getType(), e);
             return null;
         }
     }
@@ -135,12 +132,12 @@ public class JsonUtil {
             return null;
         }
         try {
-            return (T)(typeReference.getType().equals(byte[].class) ? bytes : objectMapper.readValue(bytes,
-                typeReference));
-        }
-        catch (Exception e) {
-            logger.error("Parse byte[] to Object error\nbyte[]: {}\nTypeReference<T>: {}\nError: {}", bytes,
-                typeReference.getType(), e);
+            return (T) (typeReference.getType().equals(byte[].class) ? bytes
+                    : objectMapper.readValue(bytes, typeReference));
+        } catch (Exception e) {
+            logger.error(
+                    "Parse byte[] to Object error\nbyte[]: {}\nTypeReference<T>: {}\nError: {}",
+                    bytes, typeReference.getType(), e);
             return null;
         }
     }
